d5f2880e18570cf82c251b7e1fdd15590d2b26e5
peawarrior <jacksonchen171@gmail.com>
2020-07-11 15:25:25 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,4 +1,5 @@
 class Application
+    
     def call(env)
         resp = Rack::Response.new
         req = Rack::Request.new(env)
@@ -7,13 +8,12 @@ class Application
             item = @@items.find do |item|
                 item.name == item_name
             end
-            item ? (resp.write "#{item.price}") : item_not_found
-            # if item
-            #     resp.write "#{item.price}"
-            # else
-            #     resp.write "Item not found"
-            #     resp.status = 400
-            # end
+            if item
+                resp.write "#{item.price}"
+            else
+                resp.write "Item not found"
+                resp.status = 400
+            end
         else
             resp.write "Route not found"
             resp.status = 404
@@ -21,8 +21,4 @@ class Application
         resp.finish
     end
 
-    def item_not_found
-        resp.write "Item not found"
-        resp.status = 400
-    end
 end
